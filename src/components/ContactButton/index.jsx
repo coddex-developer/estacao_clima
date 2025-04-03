4391c06c1107aadd7b7a397d5ce8f9d1193bfe57
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-04-03 20:25:19 -0300

    Ajustes no layout

--- src/components/ContactButton/index.jsx
+++ src/components/ContactButton/index.jsx
@@ -3,8 +3,8 @@ import './styles.min.css'
 function ContactButton(props) {
   return (
     <>
-      <div className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 text-center flex justify-center py-17">
-        <a className="fancy dark:border-purple-800" href={props.contact}>
+      <div className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 text-center flex justify-center pt-9 pb-8">
+        <a className="fancy" href={props.contact}>
           <span className="top-key"></span>
           <span className="text">{props.text}</span>
           <span classNane="bottom-key-1"></span>
--- src/components/MyWork/index.jsx
+++ src/components/MyWork/index.jsx
@@ -6,21 +6,21 @@ import ContactButton from '../ContactButton'
 
 function MyWork() {
   const images = [
-    { url: "../../../public/images_services/image-1.jpeg" },
-    { url: "../../../public/images_services/image-2.jpeg" },
-    { url: "../../../public/images_services/image-3.jpeg" },
-    { url: "../../../public/images_services/image-4.jpeg" },
-    { url: "../../../public/images_services/image-5.jpeg" },
-    { url: "../../../public/images_services/image-6.jpeg" },
-    { url: "../../../public/images_services/image-7.jpeg" },
-    { url: "../../../public/images_services/image-8.jpeg" },
-    { url: "../../../public/images_services/image-9.jpeg" },
-    { url: "../../../public/images_services/image-10.jpeg" }
+    { url: "/images_services/image-1.jpeg" },
+    { url: "/images_services/image-2.jpeg" },
+    { url: "/images_services/image-3.jpeg" },
+    { url: "/images_services/image-4.jpeg" },
+    { url: "/images_services/image-5.jpeg" },
+    { url: "/images_services/image-6.jpeg" },
+    { url: "/images_services/image-7.jpeg" },
+    { url: "/images_services/image-8.jpeg" },
+    { url: "/images_services/image-9.jpeg" },
+    { url: "/images_services/image-10.jpeg" }
   ];
 
   return (
     <>
-    <h2 className="text-2xl pt-14 py-2 text-white text-center bg-gray-800 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">Alguns serviços já prestados</h2>
+    <h2 className="text-2xl pt-14 pb-7 text-white text-center bg-gray-800 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">Alguns serviços já prestados</h2>
     <div className="flex overflow-hidden justify-center items-center pt-2 pb-20 bg-gray-800 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
       <Swiper
         effect="cards"

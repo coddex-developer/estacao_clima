94cf39cacf83bb89581b53d27923fb90e92ef056
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-04-06 14:44:32 -0300

    Mudando persinalizações do Navbar

--- src/components/SecondarySection/index.jsx
+++ src/components/SecondarySection/index.jsx
@@ -37,17 +37,14 @@ function SecondarySection() {
           text="Solicitar Orçamento"
         />
 
-<div className="w-full flex justify-center flex-col items-center pt-12 h-[400px] rounded-xl overflow-hidden">
-  <h3 className='titleAddres'>Nosso endereço</h3>
-  <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.019065254568!2d-122.41941568468148!3d37.77492977975895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808e0ef1b6e1%3A0x3d6e6c1b8a0d9364!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sbr!4v1616618459486!5m2!1sen!2sbr"
-    width="90%"
-    height="100%"
-    allowFullScreen=""
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade">
-  </iframe>
-</div>
+        <div className="w-full flex justify-center flex-col items-center pt-12 h-[400px] rounded-xl overflow-hidden">
+          <h3 className='titleAddres'>Nosso endereço</h3>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3844.0237294315043!2d-47.29407772487529!3d-15.536858585068146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTXCsDMyJzEyLjciUyA0N8KwMTcnMjkuNCJX!5e0!3m2!1spt-BR!2sbr!4v1743957220589!5m2!1spt-BR!2sbr" 
+         className='w-[98%] h-full'
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
       </div>
     </>
   )
